7dd793410dae44965e9609ed0d22ed9b57178e23
Whw35 <1353177916@qq.com>
2019-03-11 12:22:14 +0800

    测绘四班 伍洪委

---
+++ 2.16/2.16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=0;
+    int end1;
+    int end2;
+    int end3;
+    int end4;
+
+    cin>>a>>b;
+    end1=a+b;
+    cout<<end1<<endl;
+
+    end2=a*b;
+    cout<<end2<<endl;
+
+    end3=a-b;
+    cout<<end3<<endl;
+
+    end4=a/b;
+    cout<<end4<<endl;
+}
---
+++ 2.20/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r=0;
+    int d=0;
+    int c=0;
+    int s=0;
+
+    cin>>r;
+
+    d=2*r;
+    cout<<d<<endl;
+
+    c=3.14159*2*r;
+    cout<<c<<endl;
+
+    s=3.14159*r*r;
+    cout<<s<<endl;
+}
