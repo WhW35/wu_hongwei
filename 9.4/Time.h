13cc2016547cb8f6a9e7e2576367ef4034023f7e
Whw35 <1353177916@qq.com>
2019-05-12 18:48:20 +0800

    no message

---
+++ 9.4/Time.h
@@ -0,0 +1,18 @@
+#ifndef TIME_H
+#define TIME_H
+
+class Time
+{
+public:
+   Time(); // constructor
+   void setTime( int, int, int ); // set hour, minute and second
+   void printUniversal(); // print time in universal-time format
+   void printStandard(); // print time in standard-time format
+private:
+   int hour; // 0 - 23 (24-hour clock format)
+   int minute; // 0 - 59
+   int second; // 0 - 59
+}; // end class Time
+
+#endif
+ // TIME_H_INCLUDED
---
+++ 9.5/Complex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+Complex::Complex( double real, double imaginary )
+{
+   setComplexNumber( real, imaginary );
+} // end Complex constructor
+
+Complex Complex::add( const Complex &right )
+{
+   return Complex(
+      realPart + right.realPart, imaginaryPart + right.imaginaryPart );
+} // end function add
+
+Complex Complex::subtract( const Complex &right )
+{
+   return Complex(
+      realPart - right.realPart, imaginaryPart - right.imaginaryPart );
+} // end function subtract
+
+void Complex::printComplex()
+{
+   cout << '(' << realPart << ", " << imaginaryPart << ')';
+} // end function printComplex
+
+void Complex::setComplexNumber( double rp, double ip )
+{
+   realPart = rp;
+   imaginaryPart = ip;
+} // end function setComplexNumber
